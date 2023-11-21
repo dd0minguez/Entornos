8b3120b220d2a7c4176bae32bb0676734b6de89e
dd0minguez <148254934+dd0minguez@users.noreply.github.com>
2023-11-21 21:40:43 +0100

    Exercici1

---
+++ Activitat1.5/Exercici1.py
@@ -0,0 +1,11 @@
+Base=int(input("Base del triangulo"))
+Altura=int(input("Altura del triangulo"))
+print("El area del triangulo es: "+(Base*Altura)/2)
+
+#Algoritmo Activitat2
+#Escribir 'Base del triangulo' 
+#Leer Base
+#Escribir 'Altura del triangulo' 
+#Leer Altura
+#Escribir 'El area del triangulo es: ', (Base * Altura)/2 
+#FinAlgoritmo
