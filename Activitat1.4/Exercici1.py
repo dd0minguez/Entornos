209542e06ba1c7603b8e625bee82ab97e3ee4c2a
dd0minguez <dd0minguez@users.noreply.github.com>
2023-11-21 18:37:11 +0100

    Exercici1

---
+++ Activitat1.4/Exercici1.py
@@ -0,0 +1,18 @@
+A=input("Introduce el valor de A: ")
+B=input("Introduce el valor de B: ")
+print ("El valor A es " + A)
+print ("El valor B es " + B)
+
+
+
+
+
+
+
+#print ("Introduce el valor de A")
+#Leer A
+#print ("Introduce el valor de B")
+#Leer B
+#print ("El valor A es " + A)
+#print ("El valor B es " + B)
+
