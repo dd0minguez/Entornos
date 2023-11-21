1d36abc6e38795e82627c5602d1eb96bc7e0877e
dd0minguez <148254934+dd0minguez@users.noreply.github.com>
2023-11-21 22:04:25 +0100

    Exercici3

---
+++ Activitat1.5/Exercici3.py
@@ -0,0 +1,42 @@
+dia=input("Introduzca el dia: ")
+mes=input("Introduzca el mes; ")
+if mes=="Enero"and"Marzo"and"Mayo"and"Julio"and"Agosto"and"Octubre"and"Diciembre":
+    if dia>0 and dia < 32:
+        print("Fecha válida")
+    else:
+        print("Fecha inválida")
+if mes=="Abril"and"Junio"and"Septiembre"and"Noviembre":
+    if dia >0 and dia <31:
+        print("Fecha válida")
+    else:
+        print("Fechz inválida")
+if mes =="Febrero":
+    if dia>0 and dia <29:
+        print("Fecha válida")
+    else:
+        print("Fecha inválida")
+
+#Algoritmo Activitat2
+#Escribir 'Introduzca el dia:' 
+#Leer dia
+#Escribir 'Introduzca el mes:' 
+#Leer mes
+#Si Mayusculas(mes) = 'ENERO' O Mayusculas(mes) = 'MARZO' O Mayusculas(mes) = 'MAYO' O Mayusculas(mes) = 'JULIO' O Mayusculas(mes)= 'AGOSTO' O Mayusculas(mes) = 'OCTUBRE' O Mayusculas(mes) = 'DICIEMBRE' Entonces
+#Si dia > 0 Y dia < 32 Entonces 
+#Escribir 'Fecha válida'
+#SiNo
+#Escribir 'Fecha inválida'
+#FinSi FinSi
+#Si Mayusculas(mes) = 'ABRIL' O mes = 'JUNIO' O mes = 'SEPTIEMBRE' O mes = 'NOVIEMBRE' Entonces
+#Si dia > 0 Y dia< 31 Entonces 
+#Escribir 'Fecha válida'
+#SiNo
+#Escribir 'Fecha inválida'
+#FinSi FinSi
+#Si Mayusculas(mes) = 'FEBRERO' Entonces 
+#Si dia > 0 Y dia < 29 Entonces
+#Escribir 'Fecha válida'
+#SiNo
+#Escribir 'Fecha inválida'
+#FinSi 
+#FinAlgoritmo
