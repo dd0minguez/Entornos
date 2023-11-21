01c672ebafc2eb2b88876483bb2fe993c16a6ede
dd0minguez <148254934+dd0minguez@users.noreply.github.com>
2023-11-21 21:46:18 +0100

    exercici2

---
+++ Activitat1.5/Exercici2.py
@@ -0,0 +1,18 @@
+val1=int(input("Escriba el valor 1: "))
+val2=int(input("Escriba el valor 2: "))
+if val1>val2:
+    print("El resultado es: "+str(val1/val2))
+else:
+    print("El resultado es: "+str(val2/val1))
+
+#Algoritmo Activitat2
+#Escribir 'Escriba valor 1:' 
+#Leer val1
+#Escribir 'Escriba valor 2:' 
+#Leer val2
+#Si val1>val2
+#Escribir 'El resultado es: ' val1/val2
+#SiNo
+#Escribir 'El resultado es: ' val2/val1 
+#Fin Si
+#FinAlgoritmo
