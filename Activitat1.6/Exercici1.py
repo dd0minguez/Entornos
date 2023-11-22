741aa32514af5d8cdf8a322dc2942f5f6b5a6f77
dd0minguez <148254934+dd0minguez@users.noreply.github.com>
2023-11-22 19:05:56 +0100

    Exercici1

---
+++ Activitat1.6/Exercici1.py
@@ -0,0 +1,23 @@
+Nombre=input("Nombre del alumno ")
+Apellido1=input("Apellido paterno ")
+Apellido2=input("Apellido materno ")
+Grado=input("Grado ")
+Color=input("Color preferido ")
+Peli=input("Pelicula favorita" )
+print("El alumno "+Nombre+" "+Apellido1+" "+Apellido2+" que cursa"+Grado+" ,cuyo color favorito es el "+Color+" y la pelicula favorita es "+Peli".")
+
+#Algoritmo Activitat3
+#Escribir "Nombre del alumno" 
+#Leer Nombre
+#Escribir "Apellido paterno" 
+#Leer Apellido1
+#Escribir "Apellido materno" 
+#Leer Apellido2
+#Escribir "Grado"
+#Leer Grado
+#Escribir "Color preferido" 
+#Leer Color
+#Escribir "Película favorita" 
+#Leer Peli
+#Escribir "El alumno " Nombre " " + Apellido1 " " + Apellido2 " que cursa " Grado " , cuyo color favorito es el " Color " y la película favorita es " Peli "."
+#FinAlgoritmo
