4655c8f05b0f0be472e26ad96fbd4895948bec9a
dd0minguez <148254934+dd0minguez@users.noreply.github.com>
2023-11-22 18:54:46 +0100

    Exercici5

--- Activitat1.5/Exercici5.py
+++ Activitat1.5/Exercici5.py
@@ -1,6 +1,11 @@
 n=input("Introduce un numero para hacerlo factorial ")
 a=1
-for c  1 
+for x in c(1):
+    print("c")
+else:
+    a=a*c
+print("El resultado es: "+a)
+
 
 #Algoritmo Actividad2
 #	Definir a, b Como Real
