5a94e538085492b4212eab960cf0d7efff6e5cf3
dd0minguez <dd0minguez@users.noreply.github.com>
2023-11-22 17:29:15 +0100

    Activitat1.5

---
+++ Activitat1.5/Exercici5.py
@@ -0,0 +1,15 @@
+n=input("Introduce un numero para hacerlo factorial ")
+a=1
+for c  1 
+
+#Algoritmo Actividad2
+#	Definir a, b Como Real
+#	Escribir "Introduce un numero para hacerlo factorial"
+#	Leer n
+#	a <- 1
+#	Para c <- 1 Hasta n Con Paso 1 Hacer 
+#		Escribir c
+#		a <- a*c
+#	FinPara
+#	Escribir "El resultado es: " a
+#FinAlgoritmo
